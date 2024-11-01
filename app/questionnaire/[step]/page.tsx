f1f15093c3945f8e3e31d124c3b99946e136ad18
jebnlang <jben.lang@gmail.com>
2024-11-01 22:21:41 +0200

    Your commit message here

--- app/questionnaire/[step]/page.tsx
+++ app/questionnaire/[step]/page.tsx
@@ -1,14 +1,13 @@
 import { Suspense } from 'react'
 import Component from '@/components/Questionnaire'
 
-// Use the correct Next.js types
 type Props = {
   params: Promise<{ step: string }>
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 export default async function QuestionnairePage(props: Props) {
-    const {} = await props.params; // if you still want to destructure
+  await Promise.all([props.params, props.searchParams])
     
   return (
     <Suspense fallback={<div>Loading...</div>}>
