1d1fb54e18159c6418dedf0c31d0d98431c352ce
jebnlang <jben.lang@gmail.com>
2024-11-01 22:09:54 +0200

    Your commit message here

--- app/questionnaire/[step]/page.tsx
+++ app/questionnaire/[step]/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from 'react'
 import Component from '@/components/Questionnaire'
 
-interface Props {
+interface PageProps {
   params: {
     step: string
   }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default async function QuestionnairePage({ params }: Props) {
+export default async function QuestionnairePage({ params, searchParams }: PageProps) {
   await Promise.resolve(params)
 
   return (
