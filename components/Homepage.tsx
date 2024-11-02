f7e027a4d57e9649e308e8416a40fea059ba5456
jebnlang <jben.lang@gmail.com>
2024-11-02 21:53:09 +0200

    saving results to homepage

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
